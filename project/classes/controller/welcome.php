014a2ca11f3e3963f1a2306006f137a89a323718
Hallvard Nygard <hallvard.nygard@gmail.com>
2011-03-18 17:32:20 +0100

    Frontpage containing all of the stops

--- project/classes/controller/welcome.php
+++ project/classes/controller/welcome.php
@@ -1,10 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
+class Controller_Welcome extends Controller_Template {
 
+	public $template = 'welcome';
 	public function action_index()
 	{
-		$this->response->body('hello, world!');
+		// Just loading all stops
+		$this->template->stops = Sprig::factory('stop')->load(NULL, FALSE);
 	}
 
 } // End Welcome
---
+++ project/views/welcome.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<title>Holdeplasser i Rogaland - Hnygard.no</title>
+</head>
+
+<!--
+<script src="http://openlayers.org/api/OpenLayers.js" type="text/javascript"></script>
+<script type="text/javascript">
+	var map, layer;
+
+	function init(){
+		map = new OpenLayers.Map('map', {maxResolution:'auto'});
+		layer = new OpenLayers.Layer.WMS( "OpenLayers WMS",
+			"http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+		map.addLayer(layer);
+		map.setCenter(new OpenLayers.LonLat(0, 0), 0);
+		map.addControl(new OpenLayers.Control.LayerSwitcher());
+		var newl = new OpenLayers.Layer.GeoRSS('Holdeplasser', '/kolumbus/index.php/georss/stops/');
+		map.addLayer(newl);
+	}
+</script>-->
+
+<!-- <body onload="init()"> 
+	<!-- <div id="map" class="smallmap"></div> 
+
+	Kartdata fra OpenStreetMap. Rutedate fra Kolumbus.-->
+<body>
+<?php
+
+if(isset($stops))
+{
+	foreach ($stops as $stop)
+	{
+		echo html::anchor('holdeplass/'.$stop->stop_id.'/'.
+			url::title($stop->stop_name), $stop->stop_name).'<br>';;
+	}
+}
+?>
+
+Rutedate fra Kolumbus.
+</body>
+</html>
